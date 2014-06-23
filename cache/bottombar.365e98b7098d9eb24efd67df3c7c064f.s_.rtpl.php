38190c8a303d3290eafecc1ba326e87f383a6f89
madnerds <maditnerd@gmail.com>
2014-06-23 15:42:55 +0200

    Remove cache

--- cache/bottombar.365e98b7098d9eb24efd67df3c7c064f.s_.rtpl.php
+++
@@ -1,3 +0,0 @@
-<?php if(!class_exists('raintpl')){exit;}?><!-- Bottom Bar -->
-<div class="navbar-fixed-bottom navbar-inverse" role="navigation">
-</div>
--- cache/footer.c0d39e99431db44ba03dd64d444266cd.rtpl.php
+++
@@ -1,6 +0,0 @@
-<?php if(!class_exists('raintpl')){exit;}?> 
-<!-- CORE FOOTER -->
- <script src="./templates/bootstrap3/js/jquery.min.js"></script>
- <script src="./templates/bootstrap3/js/bootstrap.min.js"></script>
- </body>
- </html>
